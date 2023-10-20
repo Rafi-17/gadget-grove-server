b7d74c8f81242da86954bdc63c9b0b2e3e10f306
Rafi <tahmidulrafi13@gmail.com>
2023-10-20 22:49:11 +0600

    Server side Hosting done

--- index.js
+++ index.js
@@ -10,12 +10,9 @@ const port= process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// gadgetGrove
-// k1xvp2IOjbMwYAJW
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.r9pshpu.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -87,7 +84,7 @@ async function run() {
     // cart collection
     app.get('/cart', async (req, res) => {
         const cursor= cartCollections.find();
-        
+
         const result= await cursor.toArray();
         res.send(result);
       })
@@ -111,8 +108,8 @@ async function run() {
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+      // Ensures that the client will close when you finish/error
+      // await client.close();
   }
 }
 run().catch(console.dir);
