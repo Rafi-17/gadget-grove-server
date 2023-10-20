63c80b6539e5ff66faca81fabbdf8f3f0e2c45f2
Rafi <tahmidulrafi13@gmail.com>
2023-10-20 19:22:18 +0600

    app.get done for specific brand product

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express= require('express');
 const cors= require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 
 const app= express();
 const port= process.env.PORT || 5000;
@@ -30,12 +30,52 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const productCollections= client.db('productDB').collection('product');
+
+    app.get('/products', async(req,res)=>{
+        const cursor =  productCollections.find()
+        const result= await cursor.toArray();
+        console.log(result);
+        res.send(result);
+    })
+    app.get('/products/:brand',async(req,res)=>{
+        const brand= req.params.brand;
+        // console.log(brand);
+        const query= {brand: brand}
+        const cursor=  productCollections.find(query);
+        const result= await cursor.toArray();
+        res.send(result);
+    })
+    // app.get('/products/:brand', async (req, res) => {
+    //     const { brand } = req.params;
+    
+    //     try {
+    //         // Fetch products from the database where brand matches the URL parameter
+    //         const cursor = productCollections.find({ brand: brand });
+    //         const result = await cursor.toArray();
+    //         console.log(result);
+    //         res.send(result);
+    //     } catch (error) {
+    //         console.error('Error fetching products:', error);
+    //         res.status(500).json({ error: 'Internal Server Error' });
+    //     }
+    // });
+    app.post('/products', async(req, res) => {
+        const product= req.body;
+        const result= await productCollections.insertOne(product);
+        res.send(result);
+    })
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
